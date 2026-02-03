ba98834edd606a74331980778020b51039644406
Afnan <afnansaad445@gmail.com>
2026-02-03 17:24:54 +0200

    creat note Model

---
+++ lib/Models/NoteModel.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class Notemodel {
+  final String title;
+  final String subtitle;
+  final String date;
+  final int color;
+
+  Notemodel({required this.title,required this.color,required this.date,required this.subtitle});
+
+}

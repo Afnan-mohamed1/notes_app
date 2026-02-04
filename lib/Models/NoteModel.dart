cf1333aac9435079e71d5fdbac8da5e263cc92bd
Afnan <afnansaad445@gmail.com>
2026-02-04 14:22:15 +0200

    creat type adapter

--- lib/Models/NoteModel.dart
+++ lib/Models/NoteModel.dart
@@ -1,11 +1,23 @@
-import 'dart:ui';
 
-class Notemodel {
+import 'package:hive/hive.dart';
+part 'NoteModel.g.dart';
+
+@HiveType(typeId: 0)
+
+
+class notemodel extends HiveObject {
+  @HiveField(0)
   final String title;
+
+  @HiveField(1)
   final String subtitle;
+
+  @HiveField(2)
   final String date;
+
+  @HiveField(3)
   final int color;
 
-  Notemodel({required this.title,required this.color,required this.date,required this.subtitle});
+  notemodel({required this.title,required this.color,required this.date,required this.subtitle});
 
 }

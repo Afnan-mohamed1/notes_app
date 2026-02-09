7b665c4247a37e2479f6f6d26370c43ccbef3ba3
Afnan <afnansaad445@gmail.com>
2026-02-09 14:56:56 +0200

    add add note state

---
+++ lib/Cubit/Add_notes_state.dart
@@ -0,0 +1,14 @@
+part of 'Add_notes_Cubit.dart';
+
+abstract class addNoteState {}
+
+class Add_note_initial extends addNoteState{}
+class Add_note_Loading extends addNoteState{}
+class Add_note_Success extends addNoteState{}
+class Add_note_failure extends addNoteState{
+  final String errorMassage;
+
+  Add_note_failure({required this.errorMassage});
+}
+
+
